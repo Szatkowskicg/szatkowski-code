25b8b53a542dafe2ff2faf0baaad052b10ee65b3
szatkowskicg <szatkowskicg@gmail.com>
2025-08-08 02:56:44 +0200

    szatkowski-code 1.0.5

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,45 +1,7 @@
 import React from "react";
 
 const Contact = () => {
-  return (
-    <div className="container">
-      <motion.div
-        variants={{
-          hidden: {
-            opacity: 0,
-            scale: 1,
-            y: -70,
-          },
-          visible: {
-            opacity: 1,
-            scale: 1,
-            y: 0,
-          },
-        }}
-        initial="hidden"
-        animate="visible"
-        transition={{
-          ease: "easeOut",
-          duration: 0.25,
-          delay: 0.25,
-        }}
-        exit={{
-          opacity: 0,
-          x: -30,
-          transition: { ease: "easeIn", duration: 0.3 },
-        }}
-      >
-        <h1 className="h1 font-black text-center">
-          Cześć, jestem Paweł. <br />
-          Front-end developer.
-        </h1>
-        <h5 className="h5 text-center">
-          Projektowanie to dla mnie sztuka łączenia estetyki, funkcjonalności i
-          potrzeb użytkownika.
-        </h5>
-      </motion.div>
-    </div>
-  );
+  return <div className="container"></div>;
 };
 
 export default Contact;

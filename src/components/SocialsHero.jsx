c257e6c8199d9662dde7732c8ed34ae6ad37ef50
szatkowskicg <szatkowskicg@gmail.com>
2025-09-08 01:57:12 +0200

    szatkowski-code 1.0.6

--- src/components/SocialsHero.jsx
+++ src/components/SocialsHero.jsx
@@ -2,7 +2,7 @@ import { Github, Linkedin, Instagram, Facebook } from "lucide-react";
 
 const SocialsHero = () => {
   return (
-    <div className="flex flex-col space-y-4">
+    <div className="flex flex-col space-y-4 items-center justify-center">
       <a
         href="https://github.com"
         target="_blank"
@@ -36,8 +36,15 @@ const SocialsHero = () => {
         <Facebook className="w-5 h-5" />
       </a>
 
-      <span>ENG</span>
-      <span>PLN</span>
+      <span>|</span>
+
+      <span>PL</span>
+
+      <div className="flex justify-center items-center bg-n-1/10 px-4 py-2 rounded-full">
+        <p>PL</p>
+        <span className="px-2"> | </span>
+        <p>EN</p>
+      </div>
       <img src="" />
     </div>
   );
